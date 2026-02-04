f7135c176b26da84408713aeadbc98f2b57e4608
yantaiqz <yantaiqz@googlemail.com>
2026-02-04 22:10:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,9 +161,9 @@ def generate_next_turn(history):
     
     # 使用 1.5-flash，速度快且稳定
     model = genai.GenerativeModel(
-        # model_name="gemini-2.5-flash",
+        model_name="gemini-2.5-flash",
        
-        model_name='gemini-2.0-flash', 
+        # model_name='gemini-2.0-flash', 
 
         system_instruction=get_system_prompt()
     )
